6d108fa73bfb3ea8e32f037ae6c6a243b07faeb5
Anita Chavez <anita.chavez@carlsbadschools.net>
2022-04-24 16:25:49 -0600

    install npm

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -91,8 +91,9 @@ ${data.contributing}
 ${data.tests}
 
 ## Questions
-[GitHub](https://github.com/${data.github})
-${data.email}
+:pencil2: ${data.author} or
+:octocat: [${data.github}](https://github.com/${data.github}) or 
+:email: ${data.email}
 `;
 }
 
